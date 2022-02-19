de53afaafe46663a4c9a75cd0fd5f0d8aa3ff9d3
TusharJ18 <72690175+TusharJ18@users.noreply.github.com>
2022-02-19 18:51:02 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DotNetEndExamMVC.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required (ErrorMessage ="Product Id can't be empty")]
+        [Display(Name ="Product Id")]
+        public int ProductId { get; set; }
+
+        [Required (ErrorMessage="Product Name can't be empty")]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Rate can't be empty")]
+        [Display(Name = "Product Rate")]
+        public decimal Rate { get; set; }
+
+        [Required(ErrorMessage = "Description can't be empty")]
+        [Display(Name = "Product Description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Category Name can't be empty")]
+        [Display(Name = "Product's Category")]
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ ProductsController.cs
@@ -0,0 +1,129 @@
+﻿using DotNetEndExamMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetEndExamMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        [ChildActionOnly]
+        public ActionResult PartialView1()
+        {
+            return View();
+        }
+
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Product> listProd = new List<Product>();
+
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DotNetExam;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            cn.Open();
+
+            SqlCommand cmdIndex = new SqlCommand();
+            cmdIndex.Connection = cn;
+            cmdIndex.CommandType = System.Data.CommandType.StoredProcedure;
+            cmdIndex.CommandText = "IndexProcedure";
+
+
+
+        try
+        {
+                SqlDataReader dr=  cmdIndex.ExecuteReader();
+                while (dr.Read())
+                {
+                    listProd.Add(new Product { ProductId = (int)dr["ProductId"], ProductName = (string)dr["ProductName"], Rate = (decimal)dr["Rate"], Description = (string)dr["Description"], CategoryName = (string)dr["CategoryName"] });
+                } dr.Close();
+
+        } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        finally { cn.Close();  }
+
+            return View(listProd);
+        }
+
+    
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+
+        
+                SqlConnection cn = new SqlConnection();
+                cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DotNetExam;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                cn.Open();
+
+                SqlCommand cmdIndex = new SqlCommand();
+                cmdIndex.Connection = cn;
+                cmdIndex.CommandType = System.Data.CommandType.Text;
+                cmdIndex.CommandText = "Select * from Products where ProductId= @id";
+                cmdIndex.Parameters.AddWithValue("@id", id);
+
+                Product obj = null;
+            try
+            {
+                SqlDataReader dr= cmdIndex.ExecuteReader();
+                if (dr.Read())
+                {
+                    obj = new Product() { ProductId = (int)dr["ProductId"], ProductName = (string)dr["ProductName"], Rate = (decimal)dr["Rate"], Description = (string)dr["Description"], CategoryName = (string)dr["CategoryName"] };
+                } 
+                dr.Close();
+
+                return View(obj);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return RedirectToAction("Index");
+            }
+            finally { cn.Close();  }
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Product obj)
+        {
+
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DotNetExam;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            cn.Open();
+
+            SqlCommand cmdEdit = new SqlCommand();
+            cmdEdit.Connection = cn;
+            cmdEdit.CommandType = System.Data.CommandType.StoredProcedure;
+            cmdEdit.CommandText = "EditProcedure";
+
+            cmdEdit.Parameters.AddWithValue("@ProductId", id);
+            cmdEdit.Parameters.AddWithValue("@ProductName", obj.ProductName);
+            cmdEdit.Parameters.AddWithValue("@Rate", obj.Rate);
+            cmdEdit.Parameters.AddWithValue("@Description", obj.Description);
+            cmdEdit.Parameters.AddWithValue("@CategoryName", obj.CategoryName);
+
+            try
+            {
+                cmdEdit.ExecuteNonQuery();
+                return RedirectToAction("Index");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return View();
+            }
+            finally { cn.Close(); }
+        }
+
+      
+    }
+}
